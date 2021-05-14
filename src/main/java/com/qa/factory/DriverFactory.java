b76b304f246406bb97d8e1c593cf61024e7d2d35
Ajay Verma <kash.ajayverma@gmail.com>
2021-05-14 20:04:48 +0530

    Browser enum variable added

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -14,13 +14,17 @@ public class DriverFactory {
 
 	public static ThreadLocal<WebDriver> tlDriver= new ThreadLocal<>();
 
+	private enum browsers {
+		IE, CHROME, FIREFOX
+	};
+
 
 	/**
 	 * This method is to intilaize driver based on browser type
 	 * @param browser
 	 * @return Webdriver driver
 	 */
-	public WebDriver init_driver(String browser){
+	public WebDriver init_driverold(String browser){
 		System.out.println("browser value is: "+ browser);
 
 		if(browser.equalsIgnoreCase("chrome")){
@@ -41,6 +45,35 @@ public class DriverFactory {
 		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return getDriver();
 	}
+
+
+
+	public WebDriver init_driver(String browser){
+		System.out.println("browser value is: "+ browser);
+		switch (browsers.valueOf(browser.toUpperCase())){
+		case CHROME:
+			WebDriverManager.chromedriver().setup();
+			tlDriver.set(new ChromeDriver());
+			break;
+		case FIREFOX:
+			System.setProperty("webdriver.firefox.bin","C:\\Users\\ajayverma\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
+			WebDriverManager.firefoxdriver().setup();
+			tlDriver.set(new FirefoxDriver());
+			break;
+		case IE:
+			break;
+			
+		default:
+			System.out.println("Please pass the correct browser value: "+browser);
+			break;
+		}
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return getDriver();
+	}
+
+
 
 	/**
 	 * this is used to get the driver with Threadlocal
